1deb1d0b3c0ec5758ff441d6286ebddd4fc95a40
Glenn Raskovich <graskovich@gmail.com>
2019-09-07 12:37:31 -0700

    Add assets for Feed the Cat

--- src/pages/feed-the-cat.js
+++ src/pages/feed-the-cat.js
@@ -5,11 +5,12 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import CharlotteNeutral from '../images/FeedTheCat/Charlotte_Neutral.png'
-import CharlottePleased from '../images/FeedTheCat/Charlotte_Pleased.png'
-import CharlotteAngry from '../images/FeedTheCat/Charlotte_Angry.png'
-import GlennNeutral from '../images/FeedTheCat/Glenn_Neutral.png'
-import GlennPleased from '../images/FeedTheCat/Glenn_Pleased.png'
+import PaperBackground from '../images/FeedTheCat/Paper_Background.jpg'
+import CharlInert from '../images/FeedTheCat/Charlotte_Inert.png'
+import CharlHappy from '../images/FeedTheCat/Charlotte_Happy.png'
+import CharlAngry from '../images/FeedTheCat/Charlotte_Angry.png'
+import GlennInert from '../images/FeedTheCat/Glenn_Inert.png'
+import GlennHappy from '../images/FeedTheCat/Glenn_Happy.png'
 import GlennAngry from '../images/FeedTheCat/Glenn_Angry.png'
 
 const styles = {
@@ -24,30 +25,68 @@ const styles = {
   astronaut: {
     maxWidth: `300px`,
     marginBottom: `1.45rem`,
+  },
+  canvas: {
+    backgroundImage: `url(${PaperBackground})`,
+    width: `1000px`,
+    height: `600px`,
+  },
+  gameStatus: {
+    margin: 0,
+    padding: 0,
+  },
+  gameText: {
+    fontSize: `60px`,
+  },
+  titleText: {
+    fontSize: `70px`,
   }
 }
 
 const AgentState = Object.freeze({
-  "Neutral": 1,
-  "Pleased": 2,
+  "Inert": 1,
+  "Happy": 2,
   "Angry": 3
 })
 
+const maxHealth = 100
+const damage = 10
+
 const FeedTheCatPage = () => {
-  const [charlState, setCharlState] = React.useState(AgentState.Neutral);
-  const [glennState, setGlennState] = React.useState(AgentState.Neutral);
+  // Game state
+  const [sleep, SetSleep] = React.useState(true);
+  const [score, setScore] = React.useState(0);
+  const [health, setHealth] = React.useState(maxHealth);
+
+  // Agent state
+  const [charlState, setCharlState] = React.useState(AgentState.Inert);
+  const [glennState, setGlennState] = React.useState(AgentState.Inert);
+
+  let charlSrc = charlState == AgentState.Inert ? CharlInert
+                 : charlState == AgentState.Angry ? CharlAngry
+                 : CharlHappy;
+  let glennSrc = glennState == AgentState.Inert ? GlennInert
+                 : glennState == AgentState.Angry ? GlennAngry
+                 : GlennHappy;
 
   return (
   <Layout>
     <SEO title="Feed-the-Cat" />
-    <h1>Hi people</h1>
-    <p>Welcome to the game Feed the Cat!, written by Glenn Raskovich!</p>
-    <p>WIP, come back for a simple but fun game!</p>
-    {/*<div style={styles.agent}><img src={GlennPleased} /></div>*/}
-    <div style={styles.agentStatus}><img src={CharlotteAngry} /></div>
-    <div style={styles.agentStatus}><img src={GlennPleased} /></div>
-    <div style={styles.agent}><img src={CharlotteNeutral} /></div>
-    <div style={styles.agent}><img src={GlennNeutral} /></div>
+    <h1 style={styles.titleText}>Feed the Cat!</h1>
+    <table style={styles.gameStatus}>
+      <tr>
+        <td><div style={styles.agentStatus}><img src={charlSrc} /></div></td>
+        <td><div style={styles.agentStatus}><img src={glennSrc} /></div></td>
+        {/*<td></td>
+        <td></td>*/}
+      </tr>
+    </table>
+    <div style={styles.canvas}>
+      <br />
+      <p style={styles.gameText}>Score: {score}</p>
+      <div style={styles.agent}><img src={CharlInert} /></div>
+      <div style={styles.agent}><img src={GlennInert} /></div>
+    </div>
     <div style={styles.astronaut}>
       <Image />
     </div>
