728cbd05758c558b1ab275539d986807f2cbd26f
Glenn Raskovich <graskovich@gmail.com>
2020-01-16 14:00:18 -0800

    Add Tetris components

---
+++ src/components/Tetris/Stage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Cell from './Cell';
+
+const Stage = ({ stage }) => (
+  <div>Stage</div>
+)
+
+export default Stage;
---
+++ src/components/Tetris/StartButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StartButton = ({ callback }) => (
+  <div>Start!</div>
+)
+
+export default StartButton;
